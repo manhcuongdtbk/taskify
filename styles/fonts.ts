602d8c598171864c46b86b49a4cfb3ee757a3ad5
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-22 23:01:36 +0700

    Add Poppins font

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,9 +1,14 @@
 // https://nextjs.org/docs/app/api-reference/components/font#using-multiple-fonts
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import { CalSansUI } from "@calcom/cal-sans-ui/ui";
 import { CalSansText } from "@calcom/cal-sans-ui/text";
 import { CalSansGeo } from "@calcom/cal-sans-ui/geo";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const geistSans = Geist({
   variable: "--font-geist-sans",
