b47ae455eeb97bde0d31e4d57d202a96ce970c60
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-23 13:27:07 +0700

    Move ClerkProvider setup for authentication to PlatformLayout to keep RootLayout authentication-less

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import { geistSans, geistMono } from "@/fonts";
+import "@/app/globals.css";
+import { siteConfig } from "@/config/site";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@/components/clerk-provider";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: [{ url: "/logo.svg", href: "/logo.svg" }],
+};
+
+export default function PlatformLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning // https://ui.shadcn.com/docs/dark-mode/next#wrap-your-root-layout
+    >
+      <body className="flex min-h-full flex-col">
+        <ClerkProvider>
+          <ThemeProvider // https://ui.shadcn.com/docs/dark-mode/next#wrap-your-root-layout
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header className="flex h-16 items-center justify-end gap-4 p-4">
+              <Show when="signed-out">
+                <SignInButton />
+                <SignUpButton>
+                  <button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </header>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { geistSans, geistMono } from "@/fonts";
-import "./globals.css";
+import "@/app/globals.css";
 import { siteConfig } from "@/config/site";
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { ClerkProvider } from "@/components/clerk-provider";
-import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: {
@@ -24,33 +21,8 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      suppressHydrationWarning // https://ui.shadcn.com/docs/dark-mode/next#wrap-your-root-layout
     >
-      <body className="flex min-h-full flex-col">
-        <ClerkProvider>
-          <ThemeProvider // https://ui.shadcn.com/docs/dark-mode/next#wrap-your-root-layout
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <header className="flex h-16 items-center justify-end gap-4 p-4">
-              <Show when="signed-out">
-                <SignInButton />
-                <SignUpButton>
-                  <button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </Show>
-              <Show when="signed-in">
-                <UserButton />
-              </Show>
-            </header>
-            {children}
-          </ThemeProvider>
-        </ClerkProvider>
-      </body>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
