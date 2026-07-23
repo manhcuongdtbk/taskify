16f430c59c3db13117c33718811b461655a91f2d
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-23 17:39:50 +0700

    Init eslint for Clerk

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,29 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import prettier from 'eslint-config-prettier/flat'
+import prettier from "eslint-config-prettier/flat";
+import clerkNext from "@clerk/eslint-plugin/next";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
+  // https://clerk.com/docs/reference/nextjs/eslint-plugin
+  {
+    plugins: { "@clerk/next": clerkNext },
+    rules: {
+      "@clerk/next/require-auth-protection": [
+        "error",
+        {
+          protected: ["app/**"],
+          public: [
+            "app/(platform)/(clerk)/sign-in/**",
+            "app/(platform)/(clerk)/sign-up/**",
+          ],
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
