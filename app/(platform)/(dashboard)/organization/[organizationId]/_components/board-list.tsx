f152221855c5a6ef7c3cef847794a0143cb87c1e
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-24 20:36:59 +0700

    Add BoardList to organization id page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -0,0 +1,21 @@
+import { HelpCircle, User2 } from "lucide-react";
+
+export default function BoardList() {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center text-lg font-semibold text-neutral-700">
+        <User2 className="mr-2 h-6 w-6" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+        <div
+          role="button"
+          className="relative flex aspect-video h-full w-full flex-col items-center justify-center gap-y-1 rounded-sm bg-muted transition hover:opacity-75"
+        >
+          <p className="text-sm">Create new board</p>
+          <span className="text-xs">5 remaining</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,6 @@
 import { Info } from "./_components/info";
 import { Separator } from "@/components/ui/separator";
+import BoardList from "./_components/board-list";
 
 export default async function OrganizationIdPage() {
   return (
@@ -7,6 +8,7 @@ export default async function OrganizationIdPage() {
       <Info />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
+        <BoardList />
       </div>
     </div>
   );
