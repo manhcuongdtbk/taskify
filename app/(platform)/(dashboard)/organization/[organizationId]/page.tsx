575c24c9174eb07382a268eb9476b7ace0624b6d
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-24 19:01:41 +0700

    Remove board related components in organization id page before revising that page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,17 +1,8 @@
-import prisma from "@/lib/prisma";
-import Board from "./board";
-import Form from "./form";
 
 export default async function OrganizationIdPage() {
-  const boards = await prisma.board.findMany();
-
   return (
-    <div>
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} id={board.id} title={board.title} />
-        ))}
+    <div className="mb-20 w-full">
+      <div className="px-2 md:px-4">
       </div>
     </div>
   );
