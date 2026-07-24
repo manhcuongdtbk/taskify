35fa4d4fbf5799fa9f2f3c7ce0c42dc53500fdbc
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-24 17:46:37 +0700

    Migrate FormButton component for organization id page only to one of reusable form related components

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FormInput } from "@/components/form/form-input";
-import FormButton from "./form-button";
+import { FormSubmit } from "@/components/form/form-submit";
 import { createBoard } from "@/actions/create-board";
 import { useAction } from "@/hooks/use-action";
 
@@ -26,7 +26,7 @@ export default function Form() {
       <div className="flex flex-col space-y-2">
         <FormInput id="title" label="Board Title" errors={fieldErrors} />
       </div>
-      <FormButton />
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 }
