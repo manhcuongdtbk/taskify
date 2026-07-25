6cf1eaa8959182971650124d243d3c0443a8e347
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-25 19:27:07 +0700

    Use organization slug as the metadata title of organization id page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
 import OrganizationControl from "./_components/organization-control";
+import { startCase } from "es-toolkit/string";
+import { type Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { orgSlug } = await auth();
+
+  return {
+    title: startCase(orgSlug || "Organization"),
+  };
+}
 
 export default function OrganizationIdLayout({
   children,
