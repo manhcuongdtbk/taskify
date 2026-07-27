5a3574b2f5cfc4ad6c6b087dc3f7cad9f506aff6
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-27 14:24:20 +0700

    Add tanstack query package

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { geistSans, geistMono } from "@/fonts";
 import "@/app/globals.css";
 import { siteConfig } from "@/config/site";
 
+// TODO: setup tanstack query devtools. More info: https://tanstack.com/query/latest/docs/framework/react/devtools#install-and-import-the-devtools
+
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import prettier from 'eslint-config-prettier/flat'
+import prettier from "eslint-config-prettier/flat";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...pluginQuery.configs["flat/recommended-strict"],
   prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
