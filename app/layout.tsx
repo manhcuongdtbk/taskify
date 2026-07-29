02c7ea86b360a27b74459b3ebdcff82d0662f4e4
manhcuongdtbk <manhcuongdtbk@gmail.com>
2026-07-29 15:51:21 +0700

    Add @vercel/analytics dependency and integrate Analytics component into the RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { geistSans, geistMono } from "@/fonts";
 import "@/app/globals.css";
 import { siteConfig } from "@/config/site";
 import { SpeedInsights } from "@vercel/speed-insights/next"; // NOTE: https://vercel.com/docs/speed-insights/quickstart?framework=nextjs-app#add-the-speedinsights-component-to-your-app
+import { Analytics } from "@vercel/analytics/next"; // NOTE: https://vercel.com/docs/analytics/quickstart?framework=nextjs-app#add-the-analytics-component-to-your-app
 
 // TODO: setup tanstack query devtools. More info: https://tanstack.com/query/latest/docs/framework/react/devtools#install-and-import-the-devtools
 
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
